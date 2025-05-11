a6733c3e01ea7888ecca39ffd281deca4a3ebb40
BGad240 <bgad240@gmail.com>
2025-05-11 13:21:56 +0300

    add more problems solved

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 //beginner level
 
-
+//1
 const evenOrOdd = (number) => number % 2 === 0 ? console.log("even") : console.log("odd")
 evenOrOdd(5)
 
+//2 sum of digits
 function sumOfDigits(nums) {
     let def = 0;
     for (let i = 0; i < nums.length; i++) {
@@ -14,12 +15,12 @@ function sumOfDigits(nums) {
 
 console.log(sumOfDigits([1, 2, 3, 4, 5, 6, 7, -10, -14]))
 
-//reverse string
+//3 reverse string
 
 const reveresString = (string) => {
     let stReversed = ''
-    for (let i = (string.length - 1); i > 0; i-- ){
-        stReversed += string[i] 
+    for (let i = (string.length - 1); i > 0; i--) {
+        stReversed += string[i]
     }
     return stReversed
 }
@@ -27,3 +28,32 @@ const reveresString = (string) => {
 console.log(reveresString('hello world'))
 
 
+const reverseInAnotherWay = (string)=>{
+    return string.split("").reverse().join("")
+}
+
+console.log(reverseInAnotherWay("hello"))
+
+
+//4 count letters in a string
+
+const lettersCounter = (string) => {
+    return string.replace(/\s/g, "").length
+}
+console.log(lettersCounter("hello world"))
+
+//5 biggerNumberInAnArray
+
+ const biggerNumber=(arr)=>{
+     let max = arr[0]
+     for(let i = 1; i < arr.length; i++){
+         arr[i] > max? max = arr[i] : max
+     }
+     return max
+ }
+
+
+console.log(biggerNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11]))
+
+
+//6
