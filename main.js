02607777af00146deb25720c94b3df1b305860f9
BGad240 <bgad240@gmail.com>
2025-05-11 23:30:07 +0300

    average of array

--- main.js
+++ main.js
@@ -55,5 +55,14 @@ console.log(lettersCounter("hello world"))
 
 console.log(biggerNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11]))
 
+//avg of array
 
-//6
+const avg = (arr)=>{
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+         sum+=arr[i]
+    }
+    return sum / arr.length
+}
+
+console.log(avg([1,2,3,4,5]))
